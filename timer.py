01b95cf796b0a2f7f21125f69a86b08b48a2d367
Inderjeet0003 <53380463+Inderjeet0003@users.noreply.github.com>
2019-08-26 14:51:37 +0530

    Add files via upload
    Simple timer like STOPWATCH.

---
+++ timer.py
@@ -0,0 +1,20 @@
+import os
+import time
+seconds=float(0)
+minutes=int(0)
+hours=int(0)
+
+run = input("Enter R to run the program:")
+
+while run.lower()=="r":
+    if seconds >59:
+        seconds=0
+        minutes+=1
+    if minutes >59:
+        minutes=0
+        hours+=1
+    os.system('cls')
+    seconds=(seconds+ .1)
+    
+    print(hours,":",minutes,":",seconds)
+    time.sleep(.1)
